db8858464294f477a31b9ae984c809e0a2ce0241
MohammadhRimaz <mohammadhrimaz@gmail.com>
2025-02-15 16:48:11 +0530

    setup file upload from AWS S3 to backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Providers from "@/components/Providers";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,14 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
+    <ClerkProvider afterSignOutUrl="/">
+      <Providers>
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+            <Toaster />
+          </body>
+        </html>
+      </Providers>
     </ClerkProvider>
   );
 }
--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -1,27 +1,61 @@
 "use client";
 import { uploadToS3 } from "@/lib/s3";
-import { Inbox } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const UploadFile = () => {
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate, isPending } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
     onDrop: async (acceptedFiles) => {
-      console.log(acceptedFiles);
       const file = acceptedFiles[0];
       //   Restrict the file size to 10MB
       if (file.size > 10 * 1024 * 1024) {
-        alert("please upload a smaller file");
+        toast.error("File is too large!");
         return;
       }
 
       try {
+        setUploading(true);
         const data = await uploadToS3(file);
-        console.log("data", data);
+        if (!data?.file_key || !data?.file_name) {
+          toast.error("Something went wrong!");
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            console.log(data);
+            toast.success(data.message);
+          },
+          onError: (err) => {
+            toast.error("Error creating chat!");
+          },
+        });
       } catch (error) {
         console.log(error);
+      } finally {
+        setUploading(false);
       }
     },
   });
@@ -34,10 +68,20 @@ const UploadFile = () => {
         })}
       >
         <input {...getInputProps()} />
-        <>
-          <Inbox className="w-10 h-10 text-blue-500" />
-          <p className="mt-2 text-sm text-slate-500">Drop PDF Here</p>
-        </>
+        {uploading || isPending ? (
+          <>
+            {/* Loading State */}
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Spilling Tea to GPT...
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-500">Drop PDF Here</p>
+          </>
+        )}
       </div>
     </div>
   );
